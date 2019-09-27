9b60811829d7740495c8f3c45a746697712ca1cd
Simon Seyock <simonseyock@googlemail.com>
2019-09-27 16:20:33 +0200

    test for getRenderedPixelFromViewportPixel

--- test/spec/ol/renderer/layer.test.js
+++ test/spec/ol/renderer/layer.test.js
@@ -3,10 +3,18 @@ import Map from '../../../../src/ol/Map.js';
 import View from '../../../../src/ol/View.js';
 import Layer from '../../../../src/ol/layer/Layer.js';
 import TileLayer from '../../../../src/ol/layer/Tile.js';
+import VectorLayer from '../../../../src/ol/layer/Vector.js';
 import LayerRenderer from '../../../../src/ol/renderer/Layer.js';
 import XYZ from '../../../../src/ol/source/XYZ.js';
 import {fromKey} from '../../../../src/ol/tilecoord.js';
+import {Vector} from '../../../../src/ol/source.js';
+import Point from '../../../../src/ol/geom/Point.js';
+import Feature from '../../../../src/ol/Feature.js';
+import Style from '../../../../src/ol/style/Style.js';
+import Icon from '../../../../src/ol/style/Icon.js';
 
+const redPixel = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVQYV2P4z8Dw'
+  + 'HwAFAAH/plybXQAAAABJRU5ErkJggg==';
 
 describe('ol.renderer.Layer', function() {
   let renderer;
@@ -141,6 +149,66 @@ describe('ol.renderer.Layer', function() {
         done();
       });
       view.setZoom(4);
+    });
+  });
+
+  describe('getRenderedPixelFromCoordinate', function() {
+    let target, map, view, layer;
+
+    beforeEach(function(done) {
+      target = document.createElement('div');
+      Object.assign(target.style, {
+        position: 'absolute',
+        left: '-1000px',
+        top: '-1000px',
+        width: '100px',
+        height: '100px'
+      });
+      document.body.appendChild(target);
+
+      view = new View({
+        center: [0, 0],
+        multiWorld: true,
+        zoom: 17
+      });
+
+      layer = new VectorLayer({
+        source: new Vector({
+          features: [
+            new Feature(new Point([2, 0]))
+          ]
+        }),
+        style: new Style({
+          image: new Icon({
+            src: redPixel
+          })
+        })
+      });
+
+      map = new Map({
+        target: target,
+        view: view,
+        layers: [layer]
+      });
+      map.once('postrender', function() {
+        done();
+      });
+    });
+
+    afterEach(function() {
+      map.dispose();
+      document.body.removeChild(target);
+    });
+
+    it('gets the correct rendered pixel', function(done) {
+      map.getView().setRotation(0.413);
+      map.renderSync();
+      const viewPortPixel = map.getPixelFromCoordinate([2, 0]);
+      const renderedPixel = layer.getRenderedPixelFromViewportPixel(viewPortPixel);
+      const layerCanvas = map.getViewport().querySelector('.ol-layer canvas');
+      const context = layerCanvas.getContext('2d');
+      const imageData = context.getImageData(renderedPixel[0], renderedPixel[1], 1, 1);
+      expect(imageData[0]).to.be.equal(255);
     });
   });
 });
